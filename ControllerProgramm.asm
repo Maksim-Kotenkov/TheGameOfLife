11f2e0f6ebe7d4c9bf16df9c75e1cd4b4c901cfb
MurenMurenus <murenmurenus@gmail.com>
2023-03-11 17:30:12 +0700

    New code file.

---
+++ ControllerProgramm.asm
@@ -0,0 +1,59 @@
+	asect 0xf1
+IO1:
+	asect 0xf2
+IO2:
+	asect 0xf3
+IO3:
+	asect 0xf4
+IO4:
+	asect 0xf5
+IO5:
+	asect 0xf6
+IO6:
+	asect 0xf7
+IO7:
+	asect 0xf8
+IO8:
+	asect 0xf9
+IO9:
+	asect 0xfa
+IO10:
+	asect 0xfb
+IO11:
+	asect 0xfc
+IO12:
+	asect 0xfd
+IO13:
+	asect 0xfe
+IO14:
+	asect 0xff
+IO15:
+
+	asect 0x00
+start:
+
+ldi r0, IO1
+ldi r1, 0
+st r0, r1
+st r0, r1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
