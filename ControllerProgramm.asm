c5eb6d70d0e0fb744b51ae5e1595ab6426cbf98b
MurenMurenus <murenmurenus@gmail.com>
2023-04-22 13:00:40 +0300

    ALL

--- ControllerProgramm.asm
+++ ControllerProgramm.asm
@@ -51,6 +51,7 @@ MATRIX_O:
 ### main code
 asect 0
 start:
+	# Center cell, POS = 14
 	jsr init
 	# display first alive cell
 	jsr display_only_matrix_o
@@ -60,17 +61,7 @@ start:
 	jsr control
 
 	# now clear user's impact
-	ldi r0, IO1
-	ldi r2, 16
-	while
-		tst r2
-	stays pl
-		ldi r3, 0
-		st r0, r3
-		st r0, r3
-		inc r0
-		dec r2
-	wend
+	jsr clear_user_impact
 
 	# and now infinite loop of life
 	while
@@ -79,34 +70,65 @@ start:
 		tst r3
 	stays nz
 		if
-			ldi r1, 7
+			ldi r1, 9
 			cmp r1, r3
 		is eq
-			# handmade delay
-			ldi r3, IO0
-			ldi r3, IO0
-			ldi r3, IO0
 			# waiting for pause button to be pressed again
 			while
 				ldi r3, IO0
 				ld r3, r3
-				ldi r1, 7
+				ldi r1, 9
 				cmp r1, r3
 			stays ne
+				# here is possible to reset the game
+				if
+					ldi r1, 7
+					cmp r1, r3
+				is eq
+					jsr reset
+					jsr control
+					jsr clear_user_impact
+					ldi r3, 9
+				fi
 			wend
 		else
-			ldi r0, IO0
-			ldi r1, 1
-			ldi r2, 0
-			st r0, r1
-			st r0, r2
+			# reset while game is in process
+			if
+				ldi r1, 7
+				cmp r1, r3
+			is eq
+				jsr reset
+				jsr control
+				jsr clear_user_impact
+			else
+				ldi r0, IO0
+				ldi r1, 1
+				ldi r2, 0
+				st r0, r1
+				st r0, r2
+			fi
 		fi
 	wend
 
+# set all output to 0
+clear_user_impact:
+	ldi r0, IO1
+	ldi r2, 16
+	ldi r3, 0
+	while
+		tst r2
+	stays pl
+		st r0, r3
+		st r0, r3
+		inc r0
+		dec r2
+	wend
+	rts
+
+# initialize POS, cursor in MATRIX and one cell alive in MATRIX_O
 init:
-	# initialize POS, cursor in MATRIX and one cell alive in MATRIX_O
-	ldi r0, 15
-	ldi r1, 128
+	ldi r0, 14  # we place first cell in matrix[14]
+	ldi r1, 1
 	ldi r2, MATRIX_O
 	ldi r3, POS
 	st r3, r0 
@@ -120,7 +142,6 @@ init:
 	ldi r3, IO8
 	ldi r2, IO_NOW
 	st r2, r3
-
 	rts
 
 # buttons processing
@@ -134,47 +155,54 @@ control:
 		ld r0,r1 #POS in r1
 		ldi r2, MATRIX #MATRIX adress in r2
 		add r1, r2 # MATRIX adr + POS to r2
+
+		# checking button codes
 		if
 			dec r3
-		is eq
+		is eq  # 1
 			jsr up
 		fi
 		if
 			dec r3
-		is eq
+		is eq  # 2
 			jsr right
 		fi
 		if 
 			dec r3
-		is eq
+		is eq  # 3
 			jsr down
 		fi
 		if
 			dec r3
-		is eq
+		is eq  # 4
 			jsr left
 		fi
 		if
 			dec r3
-		is eq
+		is eq  # 5
 			jsr copy_to_final
 		fi
 		if
 			dec r3
-		is eq
+		is eq  # 6
 			#start playing
 			rts
 		fi
 		if
 			dec r3
-			dec r3
-		is eq
+		is eq  # 7
 			jsr reset
+		fi
+		if
+			dec r3
+		is eq  # 8
+			jsr glider
 		fi
 	wend
 	rts
 
-reset:
+# nothing special, just clear the matrix_o
+reset_matrix_o:
 	ldi r0, MATRIX_O
 	ldi r2, 32
 	ldi r3, 0
@@ -190,20 +218,55 @@ reset:
 	ld r1, r1
 	add r1, r0
 	st r0, r3
+	rts
 
-	ldi r2, 16
-	ldi r0, IO1
-	while
-		tst r2
-	stays pl
-		st r0, r3
-		st r0, r3
-		inc r0
-		dec r2
-	wend
+# reset button
+reset:
+	jsr reset_matrix_o
+	jsr clear_user_impact
 
 	jsr init
 	jsr display_only_matrix_o
+	jsr display_tick
+	rts
+
+# glider button
+glider:
+	jsr reset_matrix_o
+	jsr clear_user_impact
+
+	jsr init # cursor`ll be in center
+	ldi r3, IO7
+	ldi r0, 12
+	ld r0, r0
+	ldi r1, MATRIX_O
+	add r0, r1
+	ldi r2, 3
+	st r1, r2
+	st r3, r2
+	inc r1
+	ldi r2, 0  # second half
+	st r3, r2
+	inc r1
+
+	# next IO
+	inc r3
+	ldi r2, 5
+	st r1, r2
+	st r3, r2
+	inc r1
+	ldi r2, 0  # second half
+	st r3, r2
+
+	#next IO
+	inc r3
+	inc r1
+	ldi r2, 1
+	st r1, r2
+	st r3, r2
+	dec r2
+	st r3, r2  # 0 to the second half
+	
 	jsr display_tick
 	rts
 
@@ -221,8 +284,8 @@ copy_to_final:
 	st r1, r2
 	rts
 
-
-# if we need to remove cursor from current row
+# if we need to remove cursor from current column
+# we need only to display matrix_o to current column IO
 display_only_matrix_o:
 	ldi r1, POS
 	ld r1, r1
@@ -250,9 +313,8 @@ display_only_matrix_o:
 	
 	rts
 
-
-# display whole row (two halfs)
-display_row:
+# display whole column (two halfs)
+display_column:
 	ldi r1, POS
 	ld r1, r1
 
@@ -286,7 +348,7 @@ display_row:
 	st r2, r0
 	rts
 
-
+# right button
 right:
 	if
 		ld r2,r3
@@ -298,12 +360,12 @@ right:
 		ldi r0, 128
 		xor r0, r3
 		st r2, r3
-		jsr display_row
+		jsr display_column
 		jsr display_tick
 	fi
 	rts
 
-
+# left button
 left:
 	if
 		ld r2,r3
@@ -313,12 +375,13 @@ left:
 		jsr left_or_right
 	else
 		st r2, r3
-		jsr display_row
+		jsr display_column
 		jsr display_tick
 	fi
 	rts
 
-
+# to make code not so big, merge left and right
+# we set r3 to choose where to move
 left_or_right:
 	ldi r1, 0
 	st r2, r1
@@ -341,8 +404,6 @@ left_or_right:
 		st r1, r0
 	fi
 
-	ldi r0, POS
-	ld r0, r0
 	ldi r2, MATRIX
 	add r0, r2
 	if
@@ -353,11 +414,11 @@ left_or_right:
 		st r2, r3 # now 1 on new POS (-2 or +2)
 	fi
 
-	jsr display_row
+	jsr display_column
 	jsr display_tick
 	rts
 
-
+# up button
 up:
 	jsr display_only_matrix_o  # remove cursor from old POS
 	if
@@ -382,7 +443,7 @@ up:
 	fi
 	rts
 
-
+# down button
 down:
 	jsr display_only_matrix_o  # remove cursor from old POS
 	if
@@ -409,7 +470,7 @@ down:
 	fi
 	rts
 
-
+# same, like with right/left, but there r3 help us to move cursor to MATRIX[POS+r3]
 up_or_down:
 	ldi r0,POS
 	ld r0,r1 #POS in r1
@@ -425,10 +486,9 @@ up_or_down:
 	add r3, r2
 	st r0, r2 #overwrite POS
 
-	jsr display_row
+	jsr display_column
 	jsr display_tick
 	rts
-
 
 display_tick:
 	#display tick
